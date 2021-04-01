4c2e882b4928c8286289edac9eed4945704b14c8
Maitan <victormaitan01@gmail.com>
2021-03-31 23:18:20 -0400

    Adicionado o logo e botão de sair no drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,46 +11,26 @@
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
 
-        <q-toolbar-title>
-          Finance App
+        <q-toolbar-title class="row justify-center">
+          <q-img
+            src="https://raw.githubusercontent.com/rocketseat-education/maratona-discover-01/9d0b7435ded694abeba1e932c1f9b3f263fecbff/assets/logo.svg"
+            spinner-color="white"
+            style="height: 20px; width: 150px"
+          />
         </q-toolbar-title>
-
-        <q-btn flat class="" icon-right="exit_to_app" color="white" label="Logout" @click="$router.push({ name: 'login' })"/>
-
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      dark
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
+    <q-drawer dark v-model="leftDrawerOpen" bordered content-class="bg-grey-1">
       <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
+        <q-item-label header class="text-grey-8">
           Menu
         </q-item-label>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href=""
-        >
-          <q-item-section
-            v-if="icon"
-            avatar
-          >
-            <q-icon name="person" />
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label> Titulo</q-item-label>
-            <q-item-label caption>
-              Descrição
+        <q-item clickable @click="$router.push({ name: 'login' })">
+          <q-item-section avatar class="text-black row">
+            <q-item-label>
+              Sair do App
+              <q-icon name="exit_to_app" color="black" size="xs"/>
             </q-item-label>
           </q-item-section>
         </q-item>
@@ -64,7 +44,6 @@
 </template>
 
 <script>
-
 export default {
   name: 'MainLayout',
   data () {
